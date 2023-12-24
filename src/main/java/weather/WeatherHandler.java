afd910155280cd0ee26d0571cac960de8209fe32
Your Name <you@example.com>
2023-12-24 21:02:47 +0200

    2

--- src/main/java/weather/WeatherHandler.java
+++ src/main/java/weather/WeatherHandler.java
@@ -1,7 +1,12 @@
 package weather;
 
 import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class WeatherHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+    }
 }
